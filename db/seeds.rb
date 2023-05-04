f9dcaa507fc707dec420649297b77d2bd5f3263f
Jakub Ciupka <ciupkajakub@gmail.com>
2023-05-04 19:03:30 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+sushi_place = Restaurant.create!(name: 'Sushi Place', address: '123 Main Street')
+burger_place = Restaurant.create!(name: 'Burger Place', address: '456 Other Street')
+
+sushi_place.dishes.create!(name: 'Volcano Roll', rating: 3)
+sushi_place.dishes.create!(name: 'Salmon Nigiri', rating: 4)
+
+burger_place.dishes.create!(name: 'Barbecue Burger', rating: 5)
+burger_place.dishes.create!(name: 'Slider', rating: 3)
