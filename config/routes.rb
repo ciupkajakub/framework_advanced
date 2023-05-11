5408488df3aca053b95712cd2f8a7d039479ddb8
Jakub Ciupka <ciupkajakub@gmail.com>
2023-05-11 15:09:03 +0200

    Add api scopes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
+  scope :api do
+    scope :json_api do
+      jsonapi_resources :restaurants
+      jsonapi_resources :dishes
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
