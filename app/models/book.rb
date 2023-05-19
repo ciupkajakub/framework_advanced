1e989d6bfe9bf9f1a18c605c605980056b2a7606
Jakub Ciupka <ciupkajakub@gmail.com>
2023-05-19 21:59:12 +0200

    Add turbo stream for new books being created

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  after_create_commit -> {
+                        broadcast_prepend_to 'books', partial: 'books/book', locals: { book: self }, target: 'books'
+                      }
 end
