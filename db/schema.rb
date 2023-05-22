7e6d7fd99de4ec9aabd657a46e302ce5a762c659
Jakub Ciupka <ciupkajakub@gmail.com>
2023-05-22 15:48:49 +0200

    Add Ingredient for Pizza

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_05_22_110808) do
+ActiveRecord::Schema[7.1].define(version: 2023_05_22_131530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_05_22_110808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.bigint "pizza_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pizza_id"], name: "index_ingredients_on_pizza_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -109,5 +117,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_05_22_110808) do
   end
 
   add_foreign_key "dishes", "restaurants"
+  add_foreign_key "ingredients", "pizzas"
   add_foreign_key "items", "artists"
 end
